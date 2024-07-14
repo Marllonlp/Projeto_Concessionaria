476bd1c3737e79c816f41e8b686cc194ca44b5f8
Marlon Neves <marlonmota2019@gmail.com>
2024-07-14 15:04:27 -0300

    tela Principal

--- src/LoginCadastro.java
+++ src/LoginCadastro.java
@@ -3,52 +3,52 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-// Declaração da classe LoginCadastro, que herda da classe JFrame (para criar uma janela).
 public class LoginCadastro extends JFrame implements ActionListener {
     private JPanel painelLogin;
     private JTextField campoInserirNome;
     private JTextField campoInserirEmail;
     private JPasswordField campoInserirSenha;
+
     private JPanel painelCadastro;
 
     public LoginCadastro() {
         inicializarJanela();
     }
 
-    // Método para configurar os elementos de entrada de dados.
     private void configEntraDados() {
-        campoInserirNome = new JTextField(); //Cria um novo campo de texto chamado jlbNome.
+        campoInserirNome = new JTextField();
         campoInserirEmail = new JTextField();
         campoInserirSenha = new JPasswordField();
     }
 
-    // Método para configurar o painel jpnDados.
     private void configPaineLogin() {
         setTitle("Login");
         painelLogin = new JPanel();
-        painelLogin.setLayout(new GridLayout(7, 1, 10, 10));
-        painelLogin.setPreferredSize(new Dimension(300, 400));//jpnDados.setBackground(Color.cyan);
-        configEntraDados(); //Chama o método para configurar os elementos de entrada de dados.
+        painelLogin.setBackground(Color.gray);
+        painelLogin.setLayout(new GridLayout(7, 1, 40, 20));
+        painelLogin.setPreferredSize(new Dimension(300, 350));
 
-        JLabel title = new JLabel("Login");
-        title. setFont(new Font("Arial", Font.BOLD, 40));
+        configEntraDados();
+        JLabel title = new JLabel("Login",SwingConstants.CENTER);
+        title.setFont(new Font("Arial", Font.BOLD, 40));
         painelLogin.add(title);
 
-        JLabel jlbNome = new JLabel("NOME");
-        jlbNome.setFont(new Font("Arial", Font.ITALIC, 18));
+        JLabel jlbNome = new JLabel("CPF");
+        jlbNome.setFont(new Font("Arial", Font.BOLD, 18));
         painelLogin.add(jlbNome);
-        painelLogin.add(campoInserirNome); // Adiciona o campo de texto jlbNome ao painel.
+        painelLogin.add(campoInserirNome);
 
         JLabel jlaSenha = new JLabel("SENHA");
-        jlaSenha.setFont(new Font("Arial", Font.ITALIC, 18));
+        jlaSenha.setFont(new Font("Arial", Font.BOLD, 18));
         painelLogin.add(jlaSenha);
         painelLogin.add(campoInserirSenha);
 
-          // Criação dos botões "Entrar" e "Cadastrar"
         JButton jButtonEntrar = new JButton("Entrar");
+        jButtonEntrar.setBackground(Color.white);
+        jButtonEntrar.setFont(new Font("Arial", Font.BOLD, 18));
         JButton jButtonCadastrar = new JButton("Cadastrar");
+        jButtonCadastrar.setBackground(Color.white);
 
-          // Adiciona um evento ao botão "Cadastrar"
         jButtonCadastrar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -59,12 +59,11 @@ public class LoginCadastro extends JFrame implements ActionListener {
             }
         });
 
-// Criação de um painel para os botões
         JPanel painelBotoes = new JPanel();
         painelBotoes.setLayout(new FlowLayout());
-        painelBotoes.add(jButtonEntrar); // Adiciona o botão "Entrar" ao painel
-        painelBotoes.add(jButtonCadastrar); // Adiciona o botão "Cadastrar" ao painel
-        painelLogin.add(painelBotoes); // Adiciona o painel de botões ao painel de login
+        painelBotoes.add(jButtonEntrar);
+        painelBotoes.add(jButtonCadastrar);
+        painelLogin.add(painelBotoes);
     }
 
     private void configPainelCadastrar() {
@@ -72,9 +71,9 @@ public class LoginCadastro extends JFrame implements ActionListener {
         painelCadastro = new JPanel();
         painelCadastro.setLayout(new GridLayout(9, 1, 10, 10));
         painelCadastro.setPreferredSize(new Dimension(300, 420));
-        configEntraDados(); // Chama o método para configurar os elementos de entrada de dados.
+        configEntraDados();
 
-        JLabel title = new JLabel("Cadastrar");
+        JLabel title = new JLabel("Cadastrar",SwingConstants.CENTER);
         title.setFont(new Font("Arial", Font.BOLD, 40));
         painelCadastro.add(title);
 
@@ -87,7 +86,7 @@ public class LoginCadastro extends JFrame implements ActionListener {
         JLabel jlbEmail = new JLabel("EMAIL");
         jlbEmail.setFont(new Font("Arial", Font.ITALIC, 18));
         painelCadastro.add(jlbEmail);
-        painelCadastro.add(campoInserirEmail);// Adiciona o campo de texto jlbNome ao painel.
+        painelCadastro.add(campoInserirEmail);
 
         JLabel jlaSenha = new JLabel("SENHA");
         jlaSenha.setFont(new Font("Arial", Font.ITALIC, 18));
@@ -99,7 +98,7 @@ public class LoginCadastro extends JFrame implements ActionListener {
 
         JButton jButtonCadastrar = new JButton("Cadastrar");
 
-        jButtonEntrar.addActionListener(new ActionListener() { //adicinar evento ao botão
+        jButtonEntrar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 painelCadastro.setVisible(false);
@@ -116,28 +115,26 @@ public class LoginCadastro extends JFrame implements ActionListener {
     }
 
     private void inicializarJanela() {
-        this.setLayout(new FlowLayout()); // Define o layout da janela como FlowLayout.
-        this.setSize(new Dimension(350, 500)); // Define o tamanho da janela.
-        this.setResizable(false); // Impede que a janela seja redimensionada.
-        this.setLocationRelativeTo(null); // Centraliza a janela na tela.
+        this.setLayout(new FlowLayout());
+        this.setSize(new Dimension(350, 500));
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
 
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setVisible(true); // Torna a janela visível
 
-        configPaineLogin(); // Chama o método para configurar o painel jpnDados.
-        this.add(painelLogin); // Adiciona o painel à janela.
+        configPaineLogin();
+        this.add(painelLogin);
 
         configPainelCadastrar();
         this.add(painelCadastro);
         painelCadastro.setVisible(false);
-
-
         revalidate(); // Revalida a interface gráfica.
-
     }
-
     @Override
     public void actionPerformed(ActionEvent e) {
+
     }
+
 
 }
--- src/MenuPrincipal.java
+++ src/MenuPrincipal.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class MenuPrincipal extends JFrame {
@@ -11,80 +12,89 @@ public class MenuPrincipal extends JFrame {
 
     public void configJanela() {
         jpMenu = new JPanel();
-        jpMenu.setBackground(Color.red);
+        jpMenu.setBackground(Color.gray);
         jpMenu.setPreferredSize(new Dimension(this.getWidth(), 30));
-        jpMenu.setLayout(new BoxLayout(jpMenu, BoxLayout.LINE_AXIS)); // Altera para BoxLayout
+        jpMenu.setLayout(new BoxLayout(jpMenu, BoxLayout.LINE_AXIS)); //Altera para BoxLayout
         addBotoes();
     }
 
     public void addBotoes() {
-        JButton botaoProd = new JButton("Menu");
+        JButton botaoHome = new JButton("Home");
         jpMenu.add(Box.createHorizontalStrut(10));
-        botaoProd.setFont(new Font("Arial", Font.ITALIC, 10));
-        jpMenu.add(botaoProd);
+        botaoHome.setFont(new Font("Arial", Font.BOLD, 15));
+        botaoHome.setBackground(Color.white);
+        jpMenu.add(botaoHome);
 
         jpMenu.add(Box.createHorizontalStrut(10));
         JButton botaoSimu = new JButton("Simulado");
-        botaoSimu.setFont(new Font("Arial", Font.ITALIC, 10));
+        jpMenu.add(Box.createHorizontalStrut(2));
+        botaoSimu.setFont(new Font("Arial", Font.BOLD, 15));
+        botaoSimu.setBackground(Color.white);
         jpMenu.add(botaoSimu);
+
+        JButton botaoSobre = new JButton("Sobre");
+        jpMenu.add(Box.createHorizontalStrut(10));
+        botaoSobre.setFont(new Font("Arial", Font.BOLD, 15));
+        botaoSobre.setBackground(Color.white);
+        jpMenu.add(botaoSobre);
 
         jpMenu.add(Box.createHorizontalGlue()); // Adiciona um preenchimento horizontal flexível
 
         JButton botaoPerfil = new JButton("Perfil");
-        botaoPerfil.setFont(new Font("Arial", Font.ITALIC, 10));
+        botaoPerfil.setFont(new Font("Arial", Font.BOLD, 15));
+        botaoPerfil.setBackground(Color.white);
         jpMenu.add(botaoPerfil);
     }
 
     public void configPainelProduto() {
-        jpProdutos = new JPanel();
-        jpProdutos.setLayout(new BoxLayout(jpProdutos, BoxLayout.Y_AXIS));
+        jpProdutos = new JPanel(new GridLayout(0, 2, 10, 10)); // Altera para GridLayout com 2 colunas e espaçamento de 10px
         produtos();
     }
 
     public void produtos() {
         adicionarProduto("Porche Macan", "Descrição do Porche Macan", "imagens/porche_macan.jpg");
         adicionarProduto("Ferrari Furosangue", "Descrição da Ferrari Furosangue", "imagens/ferrari_furosangue.jpg");
+        adicionarProduto("Nissan GTR", "Descrição da Nissan GTR", "imagens/nissan_GTR.jpg");
+        adicionarProduto("BMW 320i", "Descrição da BMW 320i", "imagens/bmw_320i.jpg");
+        adicionarProduto("BMW 320i", "Descrição da BMW 320i", "imagens/bmw_320i.jpg");
     }
 
     public void adicionarProduto(String titulo, String descricao, String caminhoImagem) {
-        JPanel produtoPanel = new JPanel(new BorderLayout());
+        JPanel produtoPanel = new JPanel();
+        produtoPanel.setLayout(new BoxLayout(produtoPanel, BoxLayout.Y_AXIS));
         produtoPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
-        produtoPanel.setBackground(Color.green);
-
-        JPanel infoPanel = new JPanel();
-        infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.Y_AXIS));
+        produtoPanel.setBackground(Color.white);
 
         ImageIcon imagemIcone = new ImageIcon(caminhoImagem);
         imagemIcone = new ImageIcon(imagemIcone.getImage().getScaledInstance(350, 250, Image.SCALE_SMOOTH));
         JLabel imagemProduto = new JLabel(imagemIcone);
 
         JLabel labelTitulo = new JLabel(titulo);
-        labelTitulo.setFont(new Font("Arial", Font.BOLD, 15));
+        labelTitulo.setFont(new Font("Arial", Font.BOLD, 20));
 
         JLabel labelDescricao = new JLabel(descricao);
-        labelDescricao.setFont(new Font("Arial", Font.PLAIN, 12));
-
-        infoPanel.add(imagemProduto);
-        infoPanel.add(Box.createVerticalStrut(10)); // Espaçamento entre a imagem e o título
-        infoPanel.add(labelTitulo);
-        infoPanel.add(Box.createVerticalStrut(5)); // Espaçamento
-        infoPanel.add(labelDescricao);
+        labelDescricao.setFont(new Font("Arial", Font.PLAIN, 14));
 
         JButton botaoSimularCompra = new JButton("Simular Compra");
-        botaoSimularCompra.setFont(new Font("Arial", Font.BOLD, 12));
-        botaoSimularCompra.setBackground(Color.yellow);
+        botaoSimularCompra.setFont(new Font("Arial", Font.BOLD, 14));;
+        botaoSimularCompra.setBorder(new EmptyBorder(10,1,1,1));
+        botaoSimularCompra.setBackground(Color.lightGray);
+        botaoSimularCompra.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
-        produtoPanel.add(infoPanel, BorderLayout.CENTER);
-        produtoPanel.add(botaoSimularCompra, BorderLayout.EAST);
+        produtoPanel.add(imagemProduto);
+        produtoPanel.add(Box.createVerticalStrut(10)); // Espaçamento entre a imagem e o título
+        produtoPanel.add(labelTitulo);
+        produtoPanel.add(Box.createVerticalStrut(5)); // Espaçamento entre o título e a descrição
+        produtoPanel.add(labelDescricao);
+        produtoPanel.add(Box.createVerticalStrut(10)); // Espaçamento entre a descrição e o botão
+        produtoPanel.add(botaoSimularCompra);
         produtoPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // Margem ao redor do produto
-
         jpProdutos.add(produtoPanel);
-        jpProdutos.add(Box.createVerticalStrut(10)); // Espaçamento entre produtos
     }
 
     private void inicializarJanelaP() {
         this.setLayout(new BorderLayout());
-        this.setSize(new Dimension(800, 700));
+        this.setSize(new Dimension(798, 700));
         this.setResizable(false);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -99,7 +109,4 @@ public class MenuPrincipal extends JFrame {
         revalidate();
     }
 
-    public static void main(String[] args) {
-        new MenuPrincipal();
-    }
 }
